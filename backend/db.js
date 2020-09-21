ce9714785154982cb4b54aeccaf03c54ab3d7734
joo-hyuk <jhyuk@bgood.co.kr>
2020-09-21 19:47:55 +0900

    travis ci deploy added

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || "05081015",
-  database: process.env.MYSQL_DATABASE || "dockerapp",
-  port: process.env.MYSQL_PORT || 3306
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 })
 
 exports.pool = pool;
